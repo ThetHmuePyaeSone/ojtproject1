c093a1b1bd88c6f7500ea5f094a10eca2080c7e4
ThetHmuePyaeSone <scm.thethmuepyaesone@gmail.com>
2022-05-31 16:35:41 +0630

    change style,screen design,remove spaces

--- src/components/FirstOjtContent.vue
+++ src/components/FirstOjtContent.vue
@@ -10,7 +10,7 @@ name:'FirstComponent'
 </script>
 <style scoped>
 .firstcontent{
-    padding: 284px;
+    padding: 283px;
     margin-left: 50px;
     margin-right: 50px;
     background-color: rgb(235, 229, 236);
